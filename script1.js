db279d32119dd8e955065dfaebb4f2d93bb3d369
tomek1592 <tomek1592@gmail.com>
2016-12-18 23:47:58 +0100

    adding files

---
+++ script1.js
@@ -0,0 +1,3 @@
+var btnClick = function () {
+    document.getElementById("clickCount").innerHTML++;
+}
---
+++ script2.js
@@ -0,0 +1,5 @@
+var timer = function () {
+    document.getElementById("clickCount").innerHTML = 0;
+}
+
+setInterval(timer, 30000);
